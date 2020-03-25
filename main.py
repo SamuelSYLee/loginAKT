4296c23d92fa3c71262358a3c559db75e4dc987b
SamuelSYLee <47802461+SamuelSYLee@users.noreply.github.com>
2020-03-25 17:37:57 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,59 @@
+'----------------------------------------------------'
+'Automation for AP AnimalKenTei Website              '
+'Copyright @ Samuel Lee                              '
+'----------------------------------------------------'
+
+import time, os, csv
+from selenium import webdriver
+from public import Login
+
+class LoginTest():
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.set_window_size(1000, 1000)
+        self.driver.get("https://www.discoverychannel.com.tw/animalkentei/index.php")
+
+    def member_login(self, username, password):
+        Login().user_login(self.driver, username, password)
+        time.sleep(0.5)
+        userinfo = self.driver.find_element_by_xpath("//div[@class = 'userName']").text
+        #Login().user_logout(self.driver)
+
+        return userinfo
+
+def txtlines(filename):
+    user_info = open(filename, 'r')
+    lines = user_info.readlines()
+    user_info.close()
+    return lines
+
+def csvlines(filename):
+    lines = csv.reader(open(filename, 'r'))
+    return lines
+
+filetype = 'csv'
+filename = os.path.dirname(__file__) + '/info.' + filetype
+
+if filetype == 'txt':
+    getlines = txtlines(filename)
+elif filetype == 'csv':
+    getlines = csvlines(filename)
+
+cnt = 0
+
+for line in getlines:
+    cnt = cnt + 1
+    if filetype == 'txt':
+        username = line.split(',')[0]
+        password = line.split(',')[1]
+    elif filetype == 'csv':
+        username = line[0]
+        password = line[1]
+    
+    if cnt == 1:
+        continue
+    else:
+        userinfo = LoginTest().member_login(username, password)
+        print('Test Username: %-15s; Result: PASS' %userinfo)
---
+++ public.py
@@ -0,0 +1,28 @@
+'----------------------------------------------------'
+'Automation for AP AnimalKenTei Website              '
+'Copyright @ Samuel Lee                              '
+'----------------------------------------------------'
+
+class Login():
+    def user_login(self, driver, username, password):
+        driver.find_element_by_xpath("//button[@class='hamburger showMT']").click()
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath("//nav[@class='header-nav']/ul/li[5]").click()
+        driver.implicitly_wait(5)
+
+        AccountName = driver.find_element_by_xpath("//div[1]/input[@class='input-field w50']")
+        AccountName.clear()
+        AccountName.send_keys(username)
+
+        AccountPW = driver.find_element_by_xpath("//div[2]/input[@class='input-field w50']")
+        AccountPW.clear()
+        AccountPW.send_keys(password)
+
+        driver.find_element_by_xpath("//button[@class='btn_signin btn_blue btn_radius']").click()
+        driver.implicitly_wait(5)
+
+    def user_logout(self, driver):
+        driver.find_element_by_xpath("//button[@class='hamburger showMT']").click()
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath("//nav[@class='header-nav']/ul/li[5]").click()
+        driver.implicitly_wait(5)
